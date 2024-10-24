0dbfb7722baf33859ff7457740bfa22273c40e3b
zhang <760005931@qq.com>
2024-10-24 16:39:57 +0800

    axios二次封装

--- src/main.js
+++ src/main.js
@@ -5,7 +5,12 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
-
+//测试接口函数
+import {getCategory} from '@/apis/testAPI'
+getCategory().then(result => {
+    console.log(result);
+    
+})
 const app = createApp(App)
 
 app.use(createPinia())
