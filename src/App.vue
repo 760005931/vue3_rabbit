4a1fd298c3d7ad4b9d5a5f1fe3a9cd70705d7e29
zhang <760005931@qq.com>
2024-10-25 10:56:34 +0800

    scss文件自动导入

--- src/App.vue
+++ src/App.vue
@@ -6,4 +6,12 @@
   <!-- 一级路由出口 -->
   <RouterView />
   <el-button type="primary">Primary</el-button>
+  <div class="test">
+    test scss
+  </div>
 </template>
+<style scoped lang="scss">
+.test {
+  color: $priceColor;
+}
+</style>
