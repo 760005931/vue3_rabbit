c8babd6f91337daf1d5f2c414d556260b1574a8d
zhang <760005931@qq.com>
2024-11-06 22:19:46 +0800

    放大镜-实现大图显示以及优化

--- src/components/ImageView/index.vue
+++ src/components/ImageView/index.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref ,watch} from 'vue'
+import { ref, watch } from 'vue'
 // 图片列表
 const imageList = [
     "https://yanxuan-item.nosdn.127.net/d917c92e663c5ed0bb577c7ded73e4ec.png",
@@ -18,12 +18,16 @@ import { useMouseInElement } from '@vueuse/core'
 const target = ref(null)
 const left = ref(0)
 const top = ref(0)
+const positionX = ref(0)
+const positionY = ref(0)
 const { elementX, elementY, isOutside } = useMouseInElement(target)
 // 3.控制滑块跟随鼠标移动(监听elementX/Y变化,一旦变化,重新设置left/top值)
-watch([elementX, elementY], () => {
+watch([elementX, elementY, isOutside], () => {
+    // 如果鼠标没有移入盒子里面,直接不执行后面逻辑
+    if (isOutside.value) return
     //有效范围内控制滑块距离
     //横向
-    if (elementX.value > 100 && elementX < 300) {
+    if (elementX.value > 100 && elementX.value < 300) {
         left.value = elementX.value - 100
     }
     // 纵向
@@ -36,7 +40,11 @@ watch([elementX, elementY], () => {
 
     if (elementY.value > 300) { top.value = 200 }
     if (elementY.value < 100) { top.value = 0 }
+    //控制大图显示
+    positionX.value = -left.value * 2
+    positionY.value = -top.value * 2
 })
+
 </script>
 
 
@@ -46,7 +54,7 @@ watch([elementX, elementY], () => {
         <div class="middle" ref="target">
             <img :src="imageList[activeIndex]" alt="" />
             <!-- 蒙层小滑块 -->
-            <div class="layer" :style="{ left: `${left}px`, top: `${top}px` }"></div>
+            <div class="layer" v-show="!isOutside" :style="{ left: `${left}px`, top: `${top}px` }"></div>
         </div>
         <!-- 小图列表 -->
         <ul class="small">
@@ -59,10 +67,10 @@ watch([elementX, elementY], () => {
         <div class="large" :style="[
             {
                 backgroundImage: `url(${imageList[0]})`,
-                backgroundPositionX: `0px`,
-                backgroundPositionY: `0px`,
+                backgroundPositionX: `${positionX}px`,
+                backgroundPositionY: `${positionY}px`,
             },
-        ]" v-show="false"></div>
+        ]" v-show="!isOutside"></div>
     </div>
 </template>
 
