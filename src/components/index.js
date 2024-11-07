ae485a47ace80974c269181b022b7c236068e8fa
zhang <760005931@qq.com>
2024-11-07 14:48:14 +0800

    登录-整体认识和路由配置

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+//把component中所有组件进行全局化注册
+//通过插件方式
+import ImageView from './ImageView/index.vue'
+import XtxSku from './XtxSku/index.vue'
+export const componentPlugin = {
+    install(app) {
+        //app.component('组件名字',组件配置对象)
+        app.component('XtxImageView', ImageView)
+        app.component('XtxSku', XtxSku)
+    }
+}
