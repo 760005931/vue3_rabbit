51f8c2d908807327b0035f881c20cdd2a1be6f2d
zhang <760005931@qq.com>
2024-11-15 10:43:14 +0800

    订单-使用基础参数获取基础列表

--- src/utils/http.js
+++ src/utils/http.js
@@ -6,7 +6,7 @@ import 'element-plus/theme-chalk/el-message.css'
 import router from '@/router'
 const httpInstance = axios.create({
     baseURL: 'https://pcapi-xiaotuxian-front-devtest.itheima.net',
-    timeout: 5000
+    timeout: 10000
 })
 
 // axios请求拦截器
